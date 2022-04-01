d81cf19edfd8e4788c537f5ca28bc1646da55208
Abhijit104 <84637201+Abhijit104@users.noreply.github.com>
2022-04-01 23:13:43 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,149 @@
+import streamlit as st
+from datetime import date
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+from tqdm import tqdm
+import numpy as np
+from yahoo_fin.stock_info import get_data
+import seaborn as sns
+
+# get_data(ticker, start_date = None, end_date = None, index_as_date = True, interval = “1d”)
+
+START = "2015-01-01"
+TODAY=date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Trend Prediction')
+tickerSymbol=st.text_input('Enter stock ticker')
+
+data= get_data(tickerSymbol, start_date=START, end_date=TODAY, index_as_date = True, interval="1wk")
+data
+
+prices=data['close']
+
+returns = data['close'].pct_change().dropna()
+
+fig=plt.figure(figsize=(8,4))
+plt.plot(prices)
+plt.ylabel('Prices', fontsize=20)
+st.pyplot(fig)
+
+fig2=plt.figure(figsize=(8,4))
+plt.plot(returns)
+plt.ylabel('Return', fontsize=20)
+st.pyplot(fig2)
+
+f=plot_acf(returns)
+st.pyplot(f)
+
+q=plot_pacf(returns)
+st.pyplot(q)
+
+
+def run_simulation(returns, prices, amt, order, thresh, verbose=False, plot=True):
+    if type(order) == float:
+        thresh = None
+        
+    curr_holding = False
+    events_list = []
+    init_amt = amt
+
+    #go through dates
+    for date, r in tqdm (returns.iloc[14:].items(), total=len(returns.iloc[14:])):
+        #if you're currently holding the stock, sell it
+        if curr_holding:
+            sell_price = prices.loc[date]
+            curr_holding=False
+            ret = (sell_price-buy_price)/buy_price
+            amt *= (1+ret)
+            events_list.append(('s', date, ret))
+            
+            if verbose:
+                print('Sold at $%s'%sell_price)
+                print('Predicted Return: %s'%round(pred,4))
+                print('Actual Return: %s'%(round(ret, 4)))
+                print('=======================================')
+            continue
+
+        #get data til just before current date
+        curr_data = returns[:date]
+        
+        if type(order) == tuple:
+            try:
+                #fit model
+                model = ARIMA(curr_data, order=order).fit(maxiter=200)
+
+                #get forecast
+                pred = model.forecast()[0][0]
+
+            except:
+                pred = thresh - 1
+
+
+
+        #if you predict a high enough return and not holding, buy stock
+        if (not curr_holding) and \
+        ((type(order) == float and np.random.random() < order) 
+         or (type(order) == tuple and pred > thresh)
+         or (order == 'last' and curr_data[-1] > 0)):
+            
+            curr_holding = True
+            buy_price = prices.loc[date]
+            events_list.append(('b', date))
+            if verbose:
+                print('Bought at $%s'%buy_price)
+                
+    if verbose:
+        print('Total Amount: $%s'%round(amt,2))
+        
+    #graph
+    if plot:
+    
+        qq=plt.figure(figsize=(10,4))
+        plt.plot(prices[14:])
+
+        y_lims = (int(prices.min()*.95), int(prices.max()*1.05))
+        shaded_y_lims = int(prices.min()*.5), int(prices.max()*1.5)
+
+        for idx, event in enumerate(events_list):
+            plt.axvline(event[1], color='k', linestyle='--', alpha=0.4)
+            if event[0] == 's':
+                color = 'green' if event[2] > 0 else 'red'
+                plt.fill_betweenx(range(*shaded_y_lims), 
+                                  event[1], events_list[idx-1][1], color=color, alpha=0.1)
+
+        tot_return = round(100*(amt / init_amt - 1), 2)
+        tot_return = str(tot_return) + '%'
+        plt.title("%s Price Data\nThresh=%s\nTotal Amt: $%s\nTotal Return: %s"%(tickerSymbol, thresh, round(amt,2), tot_return), fontsize=20)
+        plt.ylim(*y_lims)
+        st.pyplot(qq)
+    
+    return amt
+
+
+st.subheader('Baseline Model : Random Buying')
+run_simulation(returns, prices, 100, 0.5, None, verbose=False)
+final_amts = [run_simulation(returns, prices, 100, 0.5, None, verbose=False, plot=False) for _ in range(1000)]
+
+ww=plt.figure(figsize=(10,4))
+sns.distplot(final_amts)
+plt.axvline(np.mean(final_amts), color='k', linestyle='--')
+plt.axvline(100, color='g', linestyle='--')
+plt.title('Avg: $%s\nSD: $%s'%(round(np.mean(final_amts),2), round(np.std(final_amts),2)), fontsize=20)
+st.pyplot(ww)
+
+st.subheader('If Last Return was Positive, Buy')
+run_simulation(returns, prices, 100, 'last', None, verbose=False)
+
+st.subheader('AR(1) Model')
+for thresh in [0, 0.001, 0.005]:
+    run_simulation(returns, prices, 100, (1,0,0), thresh, verbose=False)
+
+st.subheader('AR(5) Model')
+for thresh in [0, 0.001, 0.005]:
+    run_simulation(returns, prices, 100, (5,0,0), thresh, verbose=False)
+
+st.subheader('AR(5,5) Model')
+for thresh in [0, 0.001, 0.005]:
+    run_simulation(returns, prices, 100, (5,0,5), thresh, verbose=False)
+
